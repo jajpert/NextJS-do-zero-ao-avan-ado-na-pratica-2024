02c9448a2a06ff0823ddc3a0e28bde7e2ff31cee
Julia Ajpert <jajpert@email.com>
2024-03-21 11:08:01 -0400

    repos

--- repos/src/pages/Main/index.js
+++ repos/src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useCallback, useEffect} from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import { Container, Form, SubmitButton, List, DeleteButton} from './styles';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -73,7 +74,6 @@ export default function Main(){
 
     const handleDelete = useCallback((repo) => {
         const find = repositorios.filter(r => r.name !== repo);
-        console.log(find);
         setRepositorios(find);
     }, [repositorios]);
 
@@ -107,9 +107,9 @@ export default function Main(){
                             </DeleteButton>
                             {repo.name}
                         </span>
-                        <a href=''>
+                        <Link to={`/repositorio/${encodeURIComponent(repo.name)}`}>
                             <FaBars size={20}/>
-                        </a>
+                        </Link>
                     </li>
                 ))}
             </List>
--- repos/src/routes.js
+++ repos/src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes as RoutesRD} from 'react-router-dom';
 
-import Main from './pages/Main'
-import Repositorio from './pages/Repositorio'
+import Main from './pages/Main';
+import Repositorio from './pages/Repositorio';
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <RoutesRD>
                 <Route exact path='/' element={<Main/>}/>
-                <Route exact path='/repositorio/:repositorio' element={<Repositorio/>}/>
+                <Route exact path='/repositorio/:repoName' element={<Repositorio/>}/>
             </RoutesRD>
         </BrowserRouter>
     );
