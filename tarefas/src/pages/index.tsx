2e86db7d6b0304776ae3c4365994d442c57401dd
Julia Ajpert <jajpert@email.com>
2024-03-22 12:45:00 -0400

    tarefas

--- tarefas/src/pages/index.tsx
+++ tarefas/src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import heroImg from "@/../public/assets/hero.png";
+
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
@@ -15,7 +15,26 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.logoContent}>
           <img
+          className={styles.hero}
+          alt="Logo Tarefas"
+          src={heroImg.src}
           />
+        </div>
+
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br/>
+          seus estudos e tarefas
+        </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+
         </div>
       </main>
     </div>
