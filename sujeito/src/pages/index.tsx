0e2925834efe9da241fd6fa2e0110b9e9dfe526f
Julia Ajpert <jajpert@email.com>
2024-03-26 12:52:06 -0400

    sujeito

--- sujeito/src/pages/index.tsx
+++ sujeito/src/pages/index.tsx
@@ -1,6 +1,8 @@
-
 import styles from '@/styles/home.module.scss';
 import Head from 'next/head';
+import Image from 'next/image';
+import techImage from '@/../public/images/techs.svg';
+
 
 export default function Home() {
   return (
@@ -11,14 +13,45 @@ export default function Home() {
       <main className={styles.container}>
         <div className={styles.containerHeader}>
           <section className={styles.ctaText}>
-            <h1>Levando você ao próximo nível</h1>
-            <span>Uma plataforma com sursos que vão do zero até o profissional na prática, direto ao ponto aplicando o que usamps no mercado de trabalho</span>
+            <h1>Levando você ao próximo nível!</h1>
+            <span>Uma plataforma com sursos que vão do zero até o profissional na prática, direto ao ponto aplicando o que usamos no mercado de trabalho</span>
             <a>
               <button>COMEÇAR AGORA!</button>
             </a>
-            
-            <img src='/images/banner-conteudos.png' alt='Conteúdos Sujeito Programador'/>
           </section>
+          <img src='/images/banner-conteudos.png' alt='Conteúdos Sujeito Programador'/>
+        </div>
+        
+        <hr className={styles.divisor}/>
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Aprenda a criar aplicativos para Android e IOS</h2>
+            <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para IOS e Android, construindo aplicativos do zero até aplicações</span>
+          </section>
+          
+          <img src='/images/financasApp.png' alt='Conteúdos desenvolvimento de apps'/>
+        </div>
+
+        <hr className={styles.divisor}/>
+
+        <div className={styles.sectionContent}>
+          <img src='/images/webDev.png' alt='Conteúdos desenvolvimento de apps'/>
+          
+          <section>
+            <h2>Aprenda a criar sistemas web</h2>
+            <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado...</span>
+          </section>
+        </div>
+
+        <div className={styles.nextLevelContent}>
+          <Image src={techImage} alt='Tecnologia'/>
+
+          <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nível.</h2>
+          <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
+          <a>
+            <button>ACESSAR TURMA!</button>
+          </a>
         </div>
       </main>
     </>
