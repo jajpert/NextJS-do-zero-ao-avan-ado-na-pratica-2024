904c82e3989fc2c27a9b140275ce65ff3ec2ec2b
Julia Ajpert <jajpert@email.com>
2024-03-27 12:34:30 -0400

    sujeito

--- sujeito/src/pages/index.tsx
+++ sujeito/src/pages/index.tsx
@@ -1,10 +1,34 @@
+import { GetStaticProps } from 'next';
+
 import styles from '@/styles/home.module.scss';
 import Head from 'next/head';
+
 import Image from 'next/image';
 import techImage from '@/../public/images/techs.svg';
 
+import { getPrismicClient } from '@/services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Home() {
+
+type Content = {
+  title: string;
+  titleContent: string;
+  linkAction: string;
+  mobileContent: string;
+  mobileBanner: string;
+  webTitle: string;
+  webContent: string;
+  webBanner: string;
+}
+
+interface ContentProps{
+  content: Content
+}
+
+export default function Home({ content }: ContentProps) {
+  console.log(content);
+  
   return (
     <>
       <Head>
@@ -15,6 +39,7 @@ export default function Home() {
           <section className={styles.ctaText}>
             <h1>Levando você ao próximo nível!</h1>
             <span>Uma plataforma com sursos que vão do zero até o profissional na prática, direto ao ponto aplicando o que usamos no mercado de trabalho</span>
+            <br/>
             <a>
               <button>COMEÇAR AGORA!</button>
             </a>
@@ -56,4 +81,37 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'home')
+  ])
+
+  //console.log(response.results[0].data);
+  const {
+    title, sub_title, link_action,
+    mobile, mobile_content, mobile_banner,
+    title_web, web_content, web_banner
+  } = response.results[0].data;
+
+  const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_banner.url,
+    webTitle: RichText.asText(title_web),
+    webContent: RichText.asText(web_content),
+    webBanner: web_banner.url
+  }
+
+  return{
+    props:{
+      content
+    },
+    revalidate: 60 * 2 // a cada 2 minutos
+  }
 }
